02a16d93b9439a8b72deafc9bae0ad983874a01e
Conny Nyman <conny.nyman93@gmail.com>
2019-11-28 22:58:31 +0200

    #5 Refactored constant classes into own files.

---
+++ src/constants/DBConstants.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Conan\DataObjectUtils;
+
+class DBConstants
+{
+    const BOOLEAN = 'Boolean';
+    const CURRENCY = 'Currency';
+    const DATE = 'Date';
+    const DECIMAL = 'Decimal';
+    const HTML_TEXT = 'HTMLText';
+    const HTML_VARCHAR = 'HTMLVarchar';
+    const INT = 'Int';
+    const PERCENTAGE = 'Percentage';
+    const DATETIME = 'Datetime';
+    const TEXT = 'Text';
+    const TIME = 'Time';
+    const VARCHAR = 'Varchar';
+}
