703dc476d86938fff0030cb6a85980ed9e60675a
Conny Nyman <conny.nyman93@gmail.com>
2019-12-01 10:26:04 +0200

    #9 Added site tree field constants.

---
+++ src/constants/SiteTreeFieldConstants.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Conan\DataObjectUtils;
+
+class SiteTreeFieldConstants extends DataObjectFieldConstants
+{
+    const URL_SEGMENT = 'URLSegment';
+    const TITLE = 'Title';
+    const MENU_TITLE = 'MenuTitle';
+    const CONTENT = 'Content';
+    const META_DESCRIPTION = 'MetaDescription';
+    const EXTRA_META = 'ExtraMeta';
+    const SHOW_IN_MENUS = 'ShowInMenus';
+    const SHOW_IN_SEARCH = 'ShowInSearch';
+    const SORT = 'Sort';
+    const HAS_BROKEN_FILE = 'HasBrokenFile';
+    const HAS_BROKEN_LINK = 'HasBrokenLink';
+    const REPORT_CLASS = 'ReportClass';
+}
