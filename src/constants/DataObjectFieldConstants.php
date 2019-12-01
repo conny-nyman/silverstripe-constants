40fb32ea58054b44b4422e64a29b6cd5f40187e0
Conny Nyman <conny.nyman93@gmail.com>
2019-12-01 10:19:34 +0200

    #8 Added data object field constants.

---
+++ src/constants/DataObjectFieldConstants.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Conan\DataObjectUtils;
+
+class DataObjectFieldConstants
+{
+    const ID = 'ID';
+    const CLASS_NAME = 'ClassName';
+    const LAST_EDITED = 'LastEdited';
+    const CREATED = 'Created';
+}
