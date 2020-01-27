2067c6f39cb2a6110a5df089bdbd9b330d98b034
Conny Nyman <conny.nyman@gambitgroup.fi>
2020-01-27 10:55:53 +0200

    #38 DataObjectGroupPermissionExtension now returns null to not affect permissions if no group codes have been defined.

--- src/extensions/DataObjectGroupPermissionExtension.php
+++ src/extensions/DataObjectGroupPermissionExtension.php
@@ -11,45 +11,56 @@ class DataObjectGroupPermissionExtension extends DataExtension
     const CAN_EDIT_GROUP_CODES = 'can_edit_group_codes';
     const CAN_CREATE_GROUP_CODES = 'can_create_group_codes';
     const CAN_DELETE_GROUP_CODES = 'can_delete_group_codes';
-    const DEFAULT_GROUP_CODES = [GroupCodeConstants::ADMINISTRATORS_GROUP_CODE, GroupCodeConstants::CONTENT_AUTHORS_GROUP_CODE];
 
     /**
      * @param null $member
-     * @return bool
+     * @return bool|null
      */
-    public function canView($member = null): bool
+    public function canView($member = null): ?bool
     {
-        $can_view_group_codes = self::getConfigGroupCodesOrDefault(self::CAN_VIEW_GROUP_CODES);
+        $can_view_group_codes = self::getConfigGroupCodes(self::CAN_VIEW_GROUP_CODES);
+        if (!$can_view_group_codes) {
+            return null;
+        }
         return self::memberHasAccess($can_view_group_codes, $member);
     }
 
     /**
      * @param null $member
-     * @return bool
+     * @return bool|null
      */
-    public function canEdit($member = null): bool
+    public function canEdit($member = null): ?bool
     {
-        $can_edit_group_codes = self::getConfigGroupCodesOrDefault(self::CAN_EDIT_GROUP_CODES);
+        $can_edit_group_codes = self::getConfigGroupCodes(self::CAN_EDIT_GROUP_CODES);
+        if (!$can_edit_group_codes) {
+            return null;
+        }
         return self::memberHasAccess($can_edit_group_codes, $member);
     }
 
     /**
      * @param null $member
-     * @return bool
+     * @return bool|null
      */
-    public function canCreate($member = null): bool
+    public function canCreate($member = null): ?bool
     {
-        $can_create_group_codes = self::getConfigGroupCodesOrDefault(self::CAN_CREATE_GROUP_CODES);
+        $can_create_group_codes = self::getConfigGroupCodes(self::CAN_CREATE_GROUP_CODES);
+        if (!$can_create_group_codes) {
+            return null;
+        }
         return self::memberHasAccess($can_create_group_codes, $member);
     }
 
     /**
      * @param null $member
-     * @return bool
+     * @return bool|null
      */
-    public function canDelete($member = null): bool
+    public function canDelete($member = null): ?bool
     {
-        $can_delete_group_codes = self::getConfigGroupCodesOrDefault(self::CAN_DELETE_GROUP_CODES);
+        $can_delete_group_codes = self::getConfigGroupCodes(self::CAN_DELETE_GROUP_CODES);
+        if (!$can_delete_group_codes) {
+            return null;
+        }
         return self::memberHasAccess($can_delete_group_codes, $member);
     }
 
@@ -57,9 +68,9 @@ class DataObjectGroupPermissionExtension extends DataExtension
      * @param string $groupCodeConfigKey
      * @return array
      */
-    protected static function getConfigGroupCodesOrDefault(string $groupCodeConfigKey)
+    protected static function getConfigGroupCodes(string $groupCodeConfigKey)
     {
-        return Config::inst()->get(__CLASS__, $groupCodeConfigKey) ?: self::DEFAULT_GROUP_CODES;
+        return Config::inst()->get(__CLASS__, $groupCodeConfigKey) ?: null;
     }
 
     /**
