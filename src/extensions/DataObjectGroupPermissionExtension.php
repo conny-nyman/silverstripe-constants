cb678d6674e1d94d5526ecb922cd9c8be09156c8
Conny Nyman <conny.n@hotmail.fi>
2020-01-30 19:59:20 +0200

    #38 Added support for allow any or none in DataObjectGroupPermissionExtension.

--- src/extensions/DataObjectGroupPermissionExtension.php
+++ src/extensions/DataObjectGroupPermissionExtension.php
@@ -7,9 +7,16 @@ use SilverStripe\ORM\DataExtension;
 
 class DataObjectGroupPermissionExtension extends DataExtension
 {
+    /** @var string */
     const CAN_VIEW_GROUP_CODES = 'can_view_group_codes';
+
+    /** @var string */
     const CAN_EDIT_GROUP_CODES = 'can_edit_group_codes';
+
+    /** @var string */
     const CAN_CREATE_GROUP_CODES = 'can_create_group_codes';
+
+    /** @var string */
     const CAN_DELETE_GROUP_CODES = 'can_delete_group_codes';
 
     /**
@@ -80,7 +87,33 @@ class DataObjectGroupPermissionExtension extends DataExtension
      */
     protected static function memberHasAccess($groupCodes, $member = null): bool
     {
+        if (self::allowNone($groupCodes)) {
+            return false;
+        }
+
+        if (self::allowAny($groupCodes)) {
+            return true;
+        }
+
         $groups = GroupUtils::getGroupsFromGroupCodes($groupCodes);
         return GroupUtils::isMemberInGroups($groups, $member);
+    }
+
+    /**
+     * @param array|null $groupCodes
+     * @return bool
+     */
+    protected static function allowAny($groupCodes): bool
+    {
+        return in_array(true, $groupCodes, true);
+    }
+
+    /**
+     * @param array|null $groupCodes
+     * @return bool
+     */
+    protected static function allowNone($groupCodes): bool
+    {
+        return in_array(false, $groupCodes, true);
     }
 }
